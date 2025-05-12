1a1dba4e558a06ae2482d252eced90e3977b7248
whatacoolperson101 <shakibs3@nycstudents.net>
2025-05-12 09:57:47 -0400

    Update partA.java

--- partA.java
+++ partA.java
@@ -1,3 +1,8 @@
 public int walkDogs(int hour) {
-  
+int numDogs = numAvailableDogs(hour);
+  if(numDogs>=maxDogs) {
+  numDogs = maxDogs;
+  }
+  this.updateDogs(hour, numDogs);
+return numDogs;
 }
