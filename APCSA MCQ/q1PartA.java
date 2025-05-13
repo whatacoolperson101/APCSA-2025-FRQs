67ba7db54954fd00ea57c519cd83b650353a6109
whatacoolperson101 <shakibs3@nycstudents.net>
2025-05-13 09:46:35 -0400

    Add files via upload

---
+++ APCSA MCQ/q1PartA.java
@@ -0,0 +1,9 @@
+public int walkDogs(int hour) {
+int numDogs = company.numAvailableDogs(hour);
+  if(numDogs >= maxDogs) {
+  company.updateDog(hour, numDogs);
+    return numDogs;
+  }
+  company.updateDog(hour, maxDogs);
+return maxDogs;
+}
