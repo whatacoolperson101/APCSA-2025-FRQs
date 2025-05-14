fc72577e9e9209937a2bcc17614e3cdf9ebe28c7
whatacoolperson101 <shakibs3@nycstudents.net>
2025-05-14 09:28:17 -0400

    Update q3PartA.java

--- APCSA MCQ/q3PartA.java
+++ APCSA MCQ/q3PartA.java
@@ -1,1 +1,6 @@
-
+public Round(String[] names) {
+    competitorList = new ArrayList<>();
+    for (int i = 0; i < names.length; i++) {
+        competitorList.add(new Competitor(names[i], i + 1));
+    }
+}
