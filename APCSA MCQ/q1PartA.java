d2a01fba69589b34e6ca04071fa2bb9292fe7a72
whatacoolperson101 <shakibs3@nycstudents.net>
2025-05-15 09:53:11 -0400

    Delete APCSA MCQ/q1PartA.java

--- APCSA MCQ/q1PartA.java
+++
@@ -1,9 +0,0 @@
-public int walkDogs(int hour) {
-int numDogs = company.numAvailableDogs(hour);
-  if(numDogs >= maxDogs) {
-  company.updateDog(hour, numDogs);
-    return numDogs;
-  }
-  company.updateDog(hour, maxDogs);
-return maxDogs;
-}
