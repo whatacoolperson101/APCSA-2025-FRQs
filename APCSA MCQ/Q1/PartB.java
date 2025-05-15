57891591082e8c305f6b13cd99aa32f4ab99ef9c
whatacoolperson101 <shakibs3@nycstudents.net>
2025-05-15 09:53:25 -0400

    Update PartB.java

--- APCSA MCQ/Q1/PartB.java
+++ APCSA MCQ/Q1/PartB.java
@@ -1,1 +1,10 @@
-
+public int dogWalkShift(int startHour, int endHour) {
+  int money = 0;
+  for(int i = startHour; i<endHour; i++){
+    money += (walkDogs(i) * 5);
+    if(walkDogs(i) == maxDogs || (i >= 9 && i <= 17)) {
+      money += 3;
+    }
+  }
+  return money;
+}
